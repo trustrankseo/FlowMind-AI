5b6e439438829c2991c31de52e722c53246f4903
trustrankseo <a627377372@gmail.com>
2026-07-03 23:11:36 -0700

    Create backend/core/exceptions.py

---
+++ backend/core/exceptions.py
@@ -0,0 +1,14 @@
+from fastapi import HTTPException
+
+
+class FlowMindException(HTTPException):
+
+    def __init__(
+        self,
+        status_code: int,
+        detail: str
+    ):
+        super().__init__(
+            status_code=status_code,
+            detail=detail
+        )
